a7eb39086aff934e6fec5e7baa0a6d11e8bc8dc8
Juliel Mendonça <juliel.mendonca@gmail.com>
2023-07-10 20:14:49 -0300

    Added menu

--- Program.cs
+++ Program.cs
@@ -7,12 +7,24 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); // ToLower converte tudo para minúsculo.
+            Console.WriteLine(data);
+
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
             while (currentTime != time)
             {
@@ -21,6 +33,10 @@ namespace Stopwatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado.");
+            Thread.Sleep(1000);
         }
 
     }
