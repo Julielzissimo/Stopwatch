439462f140a2c0b8af63d8c020821f2d65b84581
Juliel Mendonça <juliel.mendonca@gmail.com>
2023-07-10 20:35:35 -0300

    calculating the time

--- Program.cs
+++ Program.cs
@@ -19,8 +19,17 @@ namespace Stopwatch
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower(); // ToLower converte tudo para minúsculo.
-            Console.WriteLine(data);
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
         }
 
         static void Start(int time)
